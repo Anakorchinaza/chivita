02487d42edd76e647f88f759c087bcc7695af3a8
Anakor Chinaza Sylvia <anakor.chinaza.s@gmail.com>
2024-05-31 09:20:06 +0100

    database and image changes

--- chivita.sql
+++ chivita.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 26, 2024 at 09:20 AM
+-- Generation Time: May 31, 2024 at 08:19 AM
 -- Server version: 8.2.0
 -- PHP Version: 8.2.13
 
@@ -255,18 +255,18 @@ CREATE TABLE IF NOT EXISTS `result` (
 --
 
 INSERT INTO `result` (`id`, `variant`, `description`, `img`, `created_at`, `updated_at`) VALUES
-(1, 'Orange Fruit Juice', 'Orange Fruit Juice is made from real natural fruits. It\'s an excellent source of Vitamin C and Magnesium you need for normal function of immune and nervous system respectively. Get transported into the luscious world of 100% natural fruits with no added sugar or preservatives served in a convenient pack to enrich your health. Make your breakfast complete with Chivita 100%', '/assets/Chivita_variant_pictures/Chivita_Orange_fruit_Juice.jpeg', '2024-05-25 23:40:52', '2024-05-26 01:19:17'),
-(2, 'Pineapple Fruit Juice', 'Pineapple Fruit Juice is made from real natural fruits. It\'s an excellent source of Calcium and Magnesium you need for normal function of digestive and nervous system respectively. Get transported into the luscious world of 100% natural fruits with no added sugar or preservatives served in a convenient pack to enrich your health. Make your breakfast complete with Chivita 100%.', '/assets/Chivita_variant_pictures/Chivita_pineapple.jpeg', '2024-05-25 23:43:04', '2024-05-26 01:19:41'),
-(3, 'Apple Fruit Juice', 'Apple Fruit Juice is made from real natural fruits. It\'s an excellent source of Calcium and Magnesium you need for normal function of digestive and nervous system respectively. Get transported into the luscious world of 100% natural fruits with no added sugar or preservatives served in a convenient pack to enrich your health. Make your breakfast complete with Chivita 100%.', '/assets/Chivita_variant_pictures/Chivita_Apple_fruit_juice.jpeg', '2024-05-25 23:45:08', '2024-05-26 01:20:00'),
-(4, 'Red Grape Fruit Juice', 'Red Grapefruit Juice is made from real natural fruits. It\'s an excellent source of Calcium and Magnesium you need for normal function of digestive and nervous system respectively. Get transported into the luscious world of 100% natural fruits with no added sugar or preservatives served in a convenient pack to enrich your health. Make your breakfast complete with Chivita 100%.', '/assets/Chivita_variant_pictures/Chivita_red_grape_fruit_juice.jpeg', '2024-05-25 23:46:40', '2024-05-26 01:20:49'),
-(5, 'Power of 6 - Mixed Citrus Juice', 'Chivita Active is a unique blend of six citrus fruits of Tangerine, Grapefruit, Lemon, Mandarine, Lime and Orange. Chivita Active is enriched with Vitamin C to energize you all day – A perfect partner on your journey to live a healthy active. ', '/assets/Chivita_variant_pictures/Chivita_Active_power_of_6_mixed_citrus_juice.jpeg', '2024-05-25 23:48:05', '2024-05-26 01:33:48'),
-(6, 'Active Zest - Mixed Fruit Drink', 'Active Zest – Mixed Fruit Drink is made from the best quality fruits and vegetables, perfectly blended with the goodness of Vitamins and nutrients. Active Zest – Mixed Fruit Drink is a healthy and tasty way to get the regular dose of antioxidants and stamina for an active healthy lifestyle.', '/assets/Chivita_variant_pictures/Chivita_active_Zest.jpeg', '2024-05-25 23:49:42', '2024-05-26 01:34:19'),
-(7, 'Power of Vegetable + Fruit (Carrot + Orange)', 'Chivita Active Vegetable Fruit Nectar Carrot & Orange is a sparkling, lovely and refreshing juice drink. It is sweet, crisp and tasty. Produced to the highest standards of quality', '/assets/Chivita_variant_pictures/Chivita_Active_Vegetable_Fruit_Nectar_Juice.jpg', '2024-05-25 23:51:06', '0000-00-00 00:00:00'),
-(8, 'Pineapple & Coconut Nectar', 'A perfect blend of Sun-ripened Pineapple and Coconut water to give you that irresistible Taste you love. Whether its drunk alone or mixed with other drinks, what you get is a delicious pleasure of exotic indulgence and an irresistible Taste. So delicious that you cannot but enjoy every moment of life with it!', '/assets/Chivita_variant_pictures/Chivita_Pineapple_and_coconut_nectar.jpg', '2024-05-25 23:52:08', '2024-05-26 01:22:24'),
-(9, 'Multifruita Nectar', 'In our quest to deliver a taste like no other, we find Chi Exotic Multifruita Nectar. Chi Exotic Multifruita is a rare and unique blend of 10 rich tropical fruits to deliver a distinctive taste that’s too good not to share.', '/assets/Chivita_variant_pictures/Chivita_multifruita_nectar.jpeg', '2024-05-25 23:53:09', '0000-00-00 00:00:00'),
-(10, 'Happy Hour Orange Safari', 'Happy Hour by Chivita is a range of refreshing still drinks. Give your mood a boost with the ever-refreshing Happy Hour. Every sip burst into flavors that guarantee to keep you smiling.', '/assets/Chivita_variant_pictures/Chivita_Happy_hour_Orange_safari.jpeg', '2024-05-25 23:54:22', '2024-05-26 01:23:47'),
-(11, 'Happy Hour Tasty Tango', 'Happy Hour by Chivita is a range of refreshing still drinks. Give your mood a boost with the ever-refreshing Happy Hour. Every sip burst into flavours that guarantee to keep you smiling.', '/assets/Chivita_variant_pictures/Chivita_Happy_hour_Tasty_Tango.jpeg', '2024-05-26 00:00:34', '2024-05-26 01:24:18'),
-(12, 'Ice Tea Lemon', 'Chivita Ice Tea is a blend of natural tea extracts with fruity flavours. Launched originally as Chi Ice Tea in 2009, its the first RTD Ice Tea to be introduced into the Nigerian market. Chivita Ice Tea is your go to drink for natural refreshment and rejuvenation. ', '/assets/Chivita_variant_pictures/Chi_Ice_Tea_pics.jpg', '2024-05-26 00:01:35', '2024-05-26 01:24:32');
+(1, 'Orange Fruit Juice', 'Orange Fruit Juice is made from real natural fruits. It\'s an excellent source of Vitamin C and Magnesium you need for normal function of immune and nervous system respectively. Get transported into the luscious world of 100% natural fruits with no added sugar or preservatives served in a convenient pack to enrich your health. Make your breakfast complete with Chivita 100%', '/assets/Chivita_variant_pictures/chivita-orange-juice.png', '2024-05-25 23:40:52', '2024-05-30 07:18:36'),
+(2, 'Pineapple Fruit Juice', 'Pineapple Fruit Juice is made from real natural fruits. It\'s an excellent source of Calcium and Magnesium you need for normal function of digestive and nervous system respectively. Get transported into the luscious world of 100% natural fruits with no added sugar or preservatives served in a convenient pack to enrich your health. Make your breakfast complete with Chivita 100%.', '/assets/Chivita_variant_pictures/chivita-pinapple-juice.png', '2024-05-25 23:43:04', '2024-05-30 07:18:57'),
+(3, 'Apple Fruit Juice', 'Apple Fruit Juice is made from real natural fruits. It\'s an excellent source of Calcium and Magnesium you need for normal function of digestive and nervous system respectively. Get transported into the luscious world of 100% natural fruits with no added sugar or preservatives served in a convenient pack to enrich your health. Make your breakfast complete with Chivita 100%.', '/assets/Chivita_variant_pictures/chivita-apple-juice1.png', '2024-05-25 23:45:08', '2024-05-30 07:19:28'),
+(4, 'Red Grape Fruit Juice', 'Red Grapefruit Juice is made from real natural fruits. It\'s an excellent source of Calcium and Magnesium you need for normal function of digestive and nervous system respectively. Get transported into the luscious world of 100% natural fruits with no added sugar or preservatives served in a convenient pack to enrich your health. Make your breakfast complete with Chivita 100%.', '/assets/Chivita_variant_pictures/chivita-red-grape-juice.png', '2024-05-25 23:46:40', '2024-05-30 07:19:46'),
+(5, 'Power of 6 - Mixed Citrus Juice', 'Chivita Active is a unique blend of six citrus fruits of Tangerine, Grapefruit, Lemon, Mandarine, Lime and Orange. Chivita Active is enriched with Vitamin C to energize you all day – A perfect partner on your journey to live a healthy active. ', '/assets/Chivita_variant_pictures/chivita-citrus-mix.png', '2024-05-25 23:48:05', '2024-05-30 07:24:18'),
+(6, 'Active Zest - Mixed Fruit Drink', 'Active Zest – Mixed Fruit Drink is made from the best quality fruits and vegetables, perfectly blended with the goodness of Vitamins and nutrients. Active Zest – Mixed Fruit Drink is a healthy and tasty way to get the regular dose of antioxidants and stamina for an active healthy lifestyle.', '/assets/Chivita_variant_pictures/active-zest.png', '2024-05-25 23:49:42', '2024-05-31 08:18:32'),
+(7, 'Power of Vegetable + Fruit (Carrot + Orange)', 'Chivita Active Vegetable Fruit Nectar Carrot & Orange is a sparkling, lovely and refreshing juice drink. It is sweet, crisp and tasty. Produced to the highest standards of quality', '/assets/Chivita_variant_pictures/prdts_powerOfVeggie.jpg', '2024-05-25 23:51:06', '2024-05-30 07:33:00'),
+(8, 'Pineapple & Coconut Nectar', 'A perfect blend of Sun-ripened Pineapple and Coconut water to give you that irresistible Taste you love. Whether its drunk alone or mixed with other drinks, what you get is a delicious pleasure of exotic indulgence and an irresistible Taste. So delicious that you cannot but enjoy every moment of life with it!', '/assets/Chivita_variant_pictures/chivita_exotic_pineapple_and_coconut.png', '2024-05-25 23:52:08', '2024-05-30 07:33:59'),
+(9, 'Multifruita Nectar', 'In our quest to deliver a taste like no other, we find Chi Exotic Multifruita Nectar. Chi Exotic Multifruita is a rare and unique blend of 10 rich tropical fruits to deliver a distinctive taste that’s too good not to share.', '/assets/Chivita_variant_pictures/chivita_exotic_multifruita_nectar.png', '2024-05-25 23:53:09', '2024-05-30 07:34:41'),
+(10, 'Happy Hour Orange Safari', 'Happy Hour by Chivita is a range of refreshing still drinks. Give your mood a boost with the ever-refreshing Happy Hour. Every sip burst into flavors that guarantee to keep you smiling.', '/assets/Chivita_variant_pictures/Chivita_HappyHour_Orange_Safari.png', '2024-05-25 23:54:22', '2024-05-30 07:35:17'),
+(11, 'Happy Hour Tasty Tango', 'Happy Hour by Chivita is a range of refreshing still drinks. Give your mood a boost with the ever-refreshing Happy Hour. Every sip burst into flavours that guarantee to keep you smiling.', '/assets/Chivita_variant_pictures/Chivita_happy_hour_tasty_tango.png', '2024-05-26 00:00:34', '2024-05-30 07:35:50'),
+(12, 'Ice Tea Lemon', 'Chivita Ice Tea is a blend of natural tea extracts with fruity flavours. Launched originally as Chi Ice Tea in 2009, its the first RTD Ice Tea to be introduced into the Nigerian market. Chivita Ice Tea is your go to drink for natural refreshment and rejuvenation. ', '/assets/Chivita_variant_pictures/Chi-Ice-Tea-1-Litre_prev_ui.png', '2024-05-26 00:01:35', '2024-05-31 08:18:04');
 
 -- --------------------------------------------------------
 
@@ -292,8 +292,7 @@ CREATE TABLE IF NOT EXISTS `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('yKflNZjCTt6Uhw4HBeEKoox2U2NiM8Pnbmh59qPz', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiTnB2V0V6eW04VEtzemNreHYxUnQ2SGRjc0hDUmdKdnZZQUF3bTlHayI7czoxODoiZmxhc2hlcjo6ZW52ZWxvcGVzIjthOjA6e31zOjk6Il9wcmV2aW91cyI7YToxOntzOjM6InVybCI7czoyMToiaHR0cDovLzEyNy4wLjAuMTo4MDAwIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1716713809),
-('jaPQYTtJuCYpXHch6Q11jIrMusU1YV5556mxmyEu', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiVUd2Vk5tZjZuZjlSelB5eXJmTG1pb1NXNnBSc21iNERkeU90NEd5cyI7czoxODoiZmxhc2hlcjo6ZW52ZWxvcGVzIjthOjA6e31zOjk6Il9wcmV2aW91cyI7YToxOntzOjM6InVybCI7czoyMToiaHR0cDovLzEyNy4wLjAuMTo4MDAwIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo3OiJ1c2VyX2lkIjtOO30=', 1716715185);
+('m4t3yxP9DUJQW1mJkoIfMGVRoWgDluyFTUdgR7fS', 8, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0', 'YTo5OntzOjY6Il90b2tlbiI7czo0MDoiNGk3MDRRNFpyQTY2VURvUGF3QnRKWTVtd3JldDUzWmQ5R0JyeVREVyI7czo3OiJ1c2VyX2lkIjtpOjg7czoxODoiZmxhc2hlcjo6ZW52ZWxvcGVzIjthOjA6e31zOjk6Il9wcmV2aW91cyI7YToxOntzOjM6InVybCI7czozMToiaHR0cDovLzEyNy4wLjAuMTo4MDAwL3RoYW5rLXlvdSI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjg7czo3OiJ2YXJpYW50IjtzOjE4OiJPcmFuZ2UgRnJ1aXQgSnVpY2UiO3M6NToiaW1hZ2UiO3M6NTc6Ii9hc3NldHMvQ2hpdml0YV92YXJpYW50X3BpY3R1cmVzL2NoaXZpdGEtb3JhbmdlLWp1aWNlLnBuZyI7czoxMToiZGVzY3JpcHRpb24iO3M6Mzc0OiJPcmFuZ2UgRnJ1aXQgSnVpY2UgaXMgbWFkZSBmcm9tIHJlYWwgbmF0dXJhbCBmcnVpdHMuIEl0J3MgYW4gZXhjZWxsZW50IHNvdXJjZSBvZiBWaXRhbWluIEMgYW5kIE1hZ25lc2l1bSB5b3UgbmVlZCBmb3Igbm9ybWFsIGZ1bmN0aW9uIG9mIGltbXVuZSBhbmQgbmVydm91cyBzeXN0ZW0gcmVzcGVjdGl2ZWx5LiBHZXQgdHJhbnNwb3J0ZWQgaW50byB0aGUgbHVzY2lvdXMgd29ybGQgb2YgMTAwJSBuYXR1cmFsIGZydWl0cyB3aXRoIG5vIGFkZGVkIHN1Z2FyIG9yIHByZXNlcnZhdGl2ZXMgc2VydmVkIGluIGEgY29udmVuaWVudCBwYWNrIHRvIGVucmljaCB5b3VyIGhlYWx0aC4gTWFrZSB5b3VyIGJyZWFrZmFzdCBjb21wbGV0ZSB3aXRoIENoaXZpdGEgMTAwJSI7fQ==', 1717054843);
 
 -- --------------------------------------------------------
 
@@ -312,7 +311,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
@@ -320,7 +319,11 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Test', 'Test@gmail.com', NULL, NULL, '2024-05-25 17:59:03', '2024-05-25 17:59:03'),
-(3, 'Chinaza Anakor', 'ruby@gmail.com', NULL, NULL, '2024-05-25 21:26:36', '2024-05-25 21:26:36');
+(5, 'adminSetting', 'chi55@gmail.com', NULL, NULL, '2024-05-26 08:21:09', '2024-05-26 08:21:09'),
+(3, 'Chinaza Anakor', 'ruby@gmail.com', NULL, NULL, '2024-05-25 21:26:36', '2024-05-25 21:26:36'),
+(6, 'Anakor Chinaza Sylvia', 'superadmin@gmail.com', NULL, NULL, '2024-05-26 08:51:48', '2024-05-26 08:51:48'),
+(7, 'Ruby Kings', 'lucy@gmail.com', NULL, NULL, '2024-05-26 08:56:58', '2024-05-26 08:56:58'),
+(8, 'Test', 'Test2@gmail.com', NULL, NULL, '2024-05-30 06:40:21', '2024-05-30 06:40:21');
 
 -- --------------------------------------------------------
 
@@ -337,7 +340,7 @@ CREATE TABLE IF NOT EXISTS `users_answer` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `users_answer`
@@ -351,7 +354,39 @@ INSERT INTO `users_answer` (`id`, `question_id`, `answer_id`, `user_id`, `create
 (5, 5, 20, 1, '2024-05-26 00:41:29', '2024-05-26 00:41:29'),
 (6, 6, 21, 1, '2024-05-26 00:41:29', '2024-05-26 00:41:29'),
 (7, 7, 26, 1, '2024-05-26 00:41:29', '2024-05-26 00:41:29'),
-(8, 8, 34, 1, '2024-05-26 00:41:29', '2024-05-26 00:41:29');
+(8, 8, 34, 1, '2024-05-26 00:41:29', '2024-05-26 00:41:29'),
+(9, 1, 1, 5, '2024-05-26 08:21:49', '2024-05-26 08:21:49'),
+(10, 2, 8, 5, '2024-05-26 08:21:49', '2024-05-26 08:21:49'),
+(11, 3, 11, 5, '2024-05-26 08:21:49', '2024-05-26 08:21:49'),
+(12, 4, 14, 5, '2024-05-26 08:21:49', '2024-05-26 08:21:49'),
+(13, 5, 17, 5, '2024-05-26 08:21:49', '2024-05-26 08:21:49'),
+(14, 6, 23, 5, '2024-05-26 08:21:49', '2024-05-26 08:21:49'),
+(15, 7, 28, 5, '2024-05-26 08:21:49', '2024-05-26 08:21:49'),
+(16, 8, 33, 5, '2024-05-26 08:21:49', '2024-05-26 08:21:49'),
+(17, 1, 3, 6, '2024-05-26 08:52:17', '2024-05-26 08:52:17'),
+(18, 2, 6, 6, '2024-05-26 08:52:17', '2024-05-26 08:52:17'),
+(19, 3, 12, 6, '2024-05-26 08:52:17', '2024-05-26 08:52:17'),
+(20, 4, 14, 6, '2024-05-26 08:52:17', '2024-05-26 08:52:17'),
+(21, 5, 17, 6, '2024-05-26 08:52:17', '2024-05-26 08:52:17'),
+(22, 6, 23, 6, '2024-05-26 08:52:17', '2024-05-26 08:52:17'),
+(23, 7, 25, 6, '2024-05-26 08:52:17', '2024-05-26 08:52:17'),
+(24, 8, 34, 6, '2024-05-26 08:52:17', '2024-05-26 08:52:17'),
+(25, 1, 1, 7, '2024-05-26 08:57:31', '2024-05-26 08:57:31'),
+(26, 2, 6, 7, '2024-05-26 08:57:31', '2024-05-26 08:57:31'),
+(27, 3, 11, 7, '2024-05-26 08:57:31', '2024-05-26 08:57:31'),
+(28, 4, 15, 7, '2024-05-26 08:57:31', '2024-05-26 08:57:31'),
+(29, 5, 19, 7, '2024-05-26 08:57:31', '2024-05-26 08:57:31'),
+(30, 6, 23, 7, '2024-05-26 08:57:31', '2024-05-26 08:57:31'),
+(31, 7, 25, 7, '2024-05-26 08:57:31', '2024-05-26 08:57:31'),
+(32, 8, 31, 7, '2024-05-26 08:57:31', '2024-05-26 08:57:31'),
+(33, 1, 1, 8, '2024-05-30 06:40:43', '2024-05-30 06:40:43'),
+(34, 2, 8, 8, '2024-05-30 06:40:43', '2024-05-30 06:40:43'),
+(35, 3, 12, 8, '2024-05-30 06:40:43', '2024-05-30 06:40:43'),
+(36, 4, 15, 8, '2024-05-30 06:40:43', '2024-05-30 06:40:43'),
+(37, 5, 18, 8, '2024-05-30 06:40:43', '2024-05-30 06:40:43'),
+(38, 6, 21, 8, '2024-05-30 06:40:43', '2024-05-30 06:40:43'),
+(39, 7, 27, 8, '2024-05-30 06:40:43', '2024-05-30 06:40:43'),
+(40, 8, 34, 8, '2024-05-30 06:40:43', '2024-05-30 06:40:43');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
